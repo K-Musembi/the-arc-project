485db121661388df649b1a7533ee197f058ad50f
K-Musembi <kevinmusembi.m@gmail.com>
2025-02-15 10:09:35 +0300

    added templates for the views

--- app/views.py
+++ app/views.py
@@ -2,11 +2,8 @@
 """views module"""
 
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model, authenticate, login as auth_login
 from django.contrib import messages
-from django.core.paginator import Paginator
-from django.db.models import Q
 from .models import Book, Borrow
 
 User = get_user_model()
